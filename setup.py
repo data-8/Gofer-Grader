1b55a4fcab8bc525bd9d750650cdd7adbaa715e3
yuvipanda <yuvipanda@gmail.com>
2018-05-18 17:05:15 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='okgrade',
-    version='0.1',
+    version='0.2',
     license='3-clause BSD',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
