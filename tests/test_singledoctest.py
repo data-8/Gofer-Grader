4ad51adf79fd84bba47da44fa8b66562902970f5
yuvipanda <yuvipanda@gmail.com>
2018-05-24 18:06:48 -0700

    Make singledoc failing test more robust
    Full text comparisons of tracebacks fail when using different paths
    for python executable. Instead, we only check that the particular
    error message we care about is still here.

--- tests/test_singledoctest.py
+++ tests/test_singledoctest.py
@@ -26,32 +26,6 @@ def test_make_test_fail():
     True
     """
     test = SingleDocTest("test", docstring)
-    # FIXME: There is a path here that will vary across installs.
-    assert test({'h': "Hello"}) == TestResult(
-        0,
-        r"""Test test failed!
-
-Test code:
-
->>> g == "Hello"
-True
-
-
-Test result:
-Trying:
-    g == "Hello"
-Expecting:
-    True
-**********************************************************************
-Line 2, in test
-Failed example:
-    g == "Hello"
-Exception raised:
-    Traceback (most recent call last):
-      File "/usr/lib/python3.6/doctest.py", line 1330, in __run
-        compileflags, 1), test.globs)
-      File "<doctest test[0]>", line 1, in <module>
-        g == "Hello"
-    NameError: name 'g' is not defined
-"""
-    )
+    tr = test({'h': "Hello"})
+    assert tr.grade == 0
+    assert "NameError: name 'g' is not defined" in tr.get_summary('text/plain')
