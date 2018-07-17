5debf1c374b4295dd54025993f800884029b633a
yuvipanda <yuvipanda@gmail.com>
2018-07-16 21:17:12 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='gradememaybe',
-    version='0.5',
+    version='0.6',
     license='3-clause BSD',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
